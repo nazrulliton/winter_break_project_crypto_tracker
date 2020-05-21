b0b56d6374f35178f5d39285d9170906f1c51757
naz_liton <nazrulliton@icloud.com>
2020-05-21 12:35:36 +0100

    previous issues correceted, modal now working and drawing correctly from the api

--- src/App.vue
+++ src/App.vue
@@ -2,72 +2,56 @@
   <div id="app">
     <div class="topnav">
       <h1>Crypto Tracker</h1>
-        <a class="active" href="#home">Home</a>
+        <a class="active" href="#">Home</a>
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a href="# about">About</a>
     </div>
     <router-view/>
-    <br>
-    <br>
+    <button type="button" class="trigger">Calc Holding</button>
+    <div>
+      <!-- <button type="button" class="trigger1"></button> -->
+    </div>
     <table class="crypto-container">
       <tr>
         <th>Crypto Name</th>
         <th>Value($)</th>
         <th>Value(£)</th>
-    </tr>
-    <tr v-for="(value, key) in cryptos">
+      </tr>
+      <tr v-bind:key="index" v-for="(value, key, index) in cryptos">
         <td>{{key}}</td>
         <td>{{value.EUR}}</td>
         <td>{{value.USD}}</td>
-    </tr>
-    <tr>
-      <button type="button" class="trigger">Analysis</button>
-    </tr>
-  </table>
+      </tr>
+    </table>
     <div class="modal">
        <div class="modal-content">
            <span class="close-button">&times;</span>
            <h1>Whats the value of my wallet?</h1>
-               <br><br>
+               <br>
                <label>Number of shares owned:</label>
                <input id="one" type="number" name="" value="">
                <br><br>
                <label>Name/Price of Coin:</label>
                <select>
-                 <option id="123" v-for="(key,value) in cryptos">{{value}}: ${{key.USD}}</option>
+                 <option id="123" v-bind:key="index" v-for="(key,value, index) in cryptos">{{value}}: ${{key.USD}}</option>
                </select>
                <br>
                <br>
-               <label>Value of coin:</label>
-               <input type="number" name="" value="" id="two">
+                <button type="" name="button" @click="add()">Submit</button>
                <br>
                <br>
                <label for="">Result:</label>
                <input type="text" id="ans" name="" value="">
-
-               <button type="" name="button" @click="add()">Submit</button>
        </div>
    </div>
-
    <div>
-
-     <!-- <ul class="news">
+    <ul class="news" id="news">
        <header class="newtitle">
-       <h2 align="left">Crypto News</h2>
+       <h2 align="left">News</h2>
           </header>
-       <li align="left" v-for="value in news">{{value.title}}<br><b>Source:</b> {{value.source}}<br><br></li>
-
-     </ul> -->
-
-
-
-     <!-- <table class="news">
-       <tr>
-         <th>Crypto News</th>
-       </tr>
-       <tr v-for="value in news"><td>{{value.title}}<br><b>Source: </b><a href="value.url">{{value.source}}</a></td></tr>
-     </table> -->
+       <li align="left" v-bind:key="index" v-for="(value, index) in news">{{value.title}}<br><b>Source:</b> <a href="www.coindesk.com">{{value.source}}</a><br><br></li>
+     </ul>
    </div>
   </div>
 </template>
@@ -85,13 +69,13 @@ export default {
   created () {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC,ZEC,XRP,BCH,LTC,DASH&tsyms=USD,EUR&api_key=4c12c3112c1df9fbfa20473e9eda953e0bb351e112affb4dd6039b2b05d72989').then(response => {
       this.cryptos = response.data
-      console.log(response)
+      // console.log(response)
     }).catch(e => {
       this.errors.push(e)
     })
     axios.get('https://min-api.cryptocompare.com/data/news/?feeds=cryptocompare,cointelegraph,coindesk&api_key=4c12c3112c1df9fbfa20473e9eda953e0bb351e112affb4dd6039b2b05d72989').then(response => {
       this.news = response.data
-      console.log(response)
+      // console.log(response)
     }).catch(e => {
       this.nerrors.push(e)
     })
@@ -99,8 +83,10 @@ export default {
   methods: {
     add () {
       var a = document.getElementById('one').value
-      var b = document.getElementById('two').value
-      var c = a * b
+      var e = document.getElementById('123').value
+      var eg = e.slice(6)
+      var example = Number(eg)
+      var c = a * example
       document.getElementById('ans').value = c
     }
   }
@@ -113,7 +99,7 @@ position: absolute;
 right: 250px;
 }
 .newtitle {
-  background-color: lightgray;
+  /* background-color: lightgray; */
   text-decoration: underline;
   font-weight: bold;
   font-size: 28px;
@@ -121,26 +107,32 @@ right: 250px;
   width: 300px; */
 }
 
-
+.news h2 {
+  text-align: center;
+}
 .news {
-  height: 900px;
+  height: 700px;
   width: 250px;
   background-color: white;
   position:inherit;
   /* left: 10px; */
   top: 0px;
-  overflow-x:auto;
+  overflow:scroll;
   list-style-type: none;
 }
 .trigger {
   font-size: 30px;
-
+  position: absolute;
+  left: 22%;
+  color: black;
+  border: 2px solid black;
 }
 /* Add a black background color to the top navigation */
 .topnav {
   background-color: white;
   overflow: hidden;
   border: 5px solid;
+  margin-bottom: 20px;
 }
 /* Style the links inside the navigation bar */
 .topnav a {
@@ -153,8 +145,8 @@ right: 250px;
 }
 /* Change the color of links on hover */
 .topnav a:hover {
-  background-color: #ddd;
-  color: black;
+  background-color: black;
+  color: white;
 }
 
 #app {
@@ -163,7 +155,7 @@ right: 250px;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 .portv{
   border: 1px solid;
@@ -218,7 +210,7 @@ right: 250px;
         transform: scale(1.0);
         transition: visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s;}
 table, th, td {
-          border: 1px solid black;
+          border: 2px solid black;
           border-collapse: collapse;
           table-layout: auto;
           text-align: center;
@@ -234,13 +226,12 @@ table {
 
           height: 50px;
           font-size: 20px;
-          font-family:
         }
 th{
-          background-color: lightgray;
+          /* background-color: lightgray; */
           text-decoration: underline;
           font-weight: bold;
-          font-size: 22px;
+          font-size: 23px;
         }
 
 </style>
